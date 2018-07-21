69f352486d2c1b62642feccadb79f9bf52c3ba8d
Dan Taylor <qubitron@users.noreply.github.com>
2018-07-22 00:06:05 +0100

    Improving query perf

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Pull a pre-built alpine docker image with nginx and python3 installed
-FROM tiangolo/uwsgi-nginx-flask:python3.6-python3.6-alpine3.7
+FROM tiangolo/uwsgi-nginx-flask:python3.6-alpine3.7
 
 ENV LISTEN_PORT=8000
 EXPOSE 8000
--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 settings = {
     'host': '',
     'master_key': '',
-    'database_id': '',
-    'collection_id': ''
+    'database_id': 'Tasks',
+    'collection_id': 'Items'
 }
 
 HOST = settings['host']
@@ -17,19 +17,18 @@ MASTER_KEY = settings['master_key']
 DATABASE_ID = settings['database_id']
 COLLECTION_ID = settings['collection_id']
 
+client = document_client.DocumentClient(HOST, {'masterKey': MASTER_KEY} )
+collection_link = 'dbs/' + DATABASE_ID + '/colls/{0}'.format(COLLECTION_ID) 
+
 @app.route('/')
 def index():
   return app.send_static_file('index.html')
 
 @app.route('/api/data')
 def get_data():
-  client = document_client.DocumentClient(HOST, {'masterKey': MASTER_KEY} )
-  collection_link = 'dbs/' + DATABASE_ID + '/colls/{0}'.format(COLLECTION_ID) 
   # The document name is hard-coded
   query_string = "SELECT * FROM c WHERE c.id = 'languages'"
   query = {'query': query_string}
-  print(query)
-
   document = list(client.QueryDocuments(collection_link, query)).pop()
   return jsonify(document)
 
